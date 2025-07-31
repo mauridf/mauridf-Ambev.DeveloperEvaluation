d1e1f885af728cc015873d91c40b24ec21b4bec5
mauridf <mauridf@gmail.com>
2025-07-31 00:07:41 -0300

    feat: correção do GetUserId e do Auth
    - Inclusão do mapeamento que estava faltando na busca de usuário por Id
    
    - Inclusão do mapeamento que estava faltando no Login

---
+++ backend/src/Ambev.DeveloperEvaluation.Application/Users/GetUser/AuthenticateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Users.GetUser
+{
+    /// <summary>
+    /// Request DTO para autenticar um usuário
+    /// </summary>
+    public class AuthenticateUserRequest
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
